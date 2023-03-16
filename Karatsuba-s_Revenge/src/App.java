c974b20e13ae0eca44abdc20c4f83ee322d803ad
Scott-Zeta <wc070211@gmail.com>
2023-03-16 18:11:43 +1030

    improve parameters

--- Karatsuba-s_Revenge/src/App.java
+++ Karatsuba-s_Revenge/src/App.java
@@ -5,103 +5,106 @@ public class App {
         Scanner sc = new Scanner(System.in);
         String[] input = sc.nextLine().split("\\s+");
         sc.close();
-        String sum = plus(input[0], input[1], input[2]);
-        String mul = multipli(input[0], input[1], input[2]);
-        // System.out.println(fillZero(input[0], input[1])[0] + " " + fillZero(input[0], input[1])[1]);
-        System.out.println(sum + " " + mul + " " + "0" );
+        int base = Integer.valueOf(input[2]);
+        String sum = plus(input[0], input[1], base);
+        String mul = multipli(input[0], input[1], base);
+        // System.out.println(fillZero(input[0], input[1])[0] + " " + fillZero(input[0],
+        // input[1])[1]);
+        System.out.println(sum + " " + mul + " " + "0");
     }
 
-    public static String plus(String n1, String n2, String baseStr){
+    public static String plus(String n1, String n2, int base) {
         Stack<Character> number1 = new Stack<>();
         Stack<Character> number2 = new Stack<>();
         StringBuilder sb = new StringBuilder();
         int buffer = 0;
-        int base = Integer.valueOf(baseStr);
 
-        for(char c: n1.toCharArray()){
+        for (char c : n1.toCharArray()) {
             number1.push(c);
         }
-        for(char c: n2.toCharArray()){
+        for (char c : n2.toCharArray()) {
             number2.push(c);
         }
 
-        while(!number1.isEmpty() && !number2.isEmpty()){
+        while (!number1.isEmpty() && !number2.isEmpty()) {
             int d1 = number1.pop() - '0';
             int d2 = number2.pop() - '0';
             int sum = d1 + d2 + buffer;
-            if (sum >= base){
+            if (sum >= base) {
                 sum = sum - base;
                 buffer = 1;
-            }else{
+            } else {
                 buffer = 0;
             }
-            sb.append((char)(sum + '0'));
+            sb.append((char) (sum + '0'));
         }
 
-        while(!number1.isEmpty()){
+        while (!number1.isEmpty()) {
             int d1 = number1.pop() - '0';
             int sum = d1 + buffer;
-            if (sum >= base){
+            if (sum >= base) {
                 sum = sum - base;
                 buffer = 1;
-            }else{
+            } else {
                 buffer = 0;
             }
-            sb.append((char)(sum + '0'));
+            sb.append((char) (sum + '0'));
         }
-        while(!number2.isEmpty()){
+        while (!number2.isEmpty()) {
             int d2 = number2.pop() - '0';
             int sum = d2 + buffer;
-            if (sum >= base){
+            if (sum >= base) {
                 sum = sum - base;
                 buffer = 1;
-            }else{
+            } else {
                 buffer = 0;
             }
-            sb.append((char)(sum + '0'));
+            sb.append((char) (sum + '0'));
         }
 
-        if(buffer != 0){
-            sb.append((char)(buffer + '0'));
+        if (buffer != 0) {
+            sb.append((char) (buffer + '0'));
         }
 
         sb.reverse();
         return sb.toString();
     }
 
-    public static String multipli(String n1, String n2, String baseStr){
-        int base = Integer.valueOf(baseStr);
-        String[] fillZero = fillZero(n1, n2);
-        n1 = fillZero[0];
-        n2 = fillZero[1];
+    public static String multipli(String n1, String n2, int base) {
+        if (n1.length() != n2.length()) {
+            String[] fillZero = fillZero(n1, n2);
+            n1 = fillZero[0];
+            n2 = fillZero[1];
+        }
+
 
         return null;
     }
 
-    public static String[] fillZero(String n1, String n2){
+    public static String[] fillZero(String n1, String n2) {
         StringBuilder sb = new StringBuilder();
         int d1 = n1.length();
         int d2 = n2.length();
-        
-        if(d1>d2){
-            for(int i=0; i < d1-d2; i++){
+
+        if (d1 > d2) {
+            for (int i = 0; i < d1 - d2; i++) {
                 sb.append('0');
             }
             sb.append(n2);
             n2 = sb.toString();
         }
-        if(d2>d1){
-            for(int i=0; i < d2-d1; i++){
+        if (d2 > d1) {
+            for (int i = 0; i < d2 - d1; i++) {
                 sb.append('0');
             }
             sb.append(n1);
             n1 = sb.toString();
         }
-        return new String[] {n1,n2};
+        return new String[] { n1, n2 };
     }
 
-    public static String mulDigit(int d1, int d2, int base){
-        int result_NBase = (d1*d2)/base + (d1*d2)%base;
+    public static String mulDigit(int d1, int d2, int base) {
+        int result_NBase = (d1 * d2) / base + (d1 * d2) % base;
         return Integer.toString(result_NBase);
     }
 

8a565f47e18507ecc56e91b0ac45c44e1f112d61
Scott-Zeta <wc070211@gmail.com>
2023-03-16 19:31:33 +1030

    implement multiplication

--- Karatsuba-s_Revenge/src/App.java
+++ Karatsuba-s_Revenge/src/App.java
@@ -71,14 +71,44 @@ public class App {
     }
 
     public static String multipli(String n1, String n2, int base) {
-        if (n1.length() != n2.length()) {
+        int d1 = n1.length();
+        int d2 = n2.length();
+        if (d1 != d2) {
             String[] fillZero = fillZero(n1, n2);
             n1 = fillZero[0];
             n2 = fillZero[1];
         }
+        if (d1 == 1 && d2 == 1) {
+            String result = mulDigit(Integer.valueOf(n1), Integer.valueOf(n2), base);
+            return result;
+        }
+        int mid = n1.length() / 2;
 
+        String a = n1.substring(0, mid);
+        String b = n1.substring(mid);
+        String c = n2.substring(0, mid);
+        String d = n2.substring(mid);
 
-        return null;
+        String ac = multipli(a, c, base);
+        String bd = multipli(b, d, base);
+        String ad = multipli(a, d, base);
+        String bc = multipli(b, c, base);
+
+        StringBuilder sbFirst = new StringBuilder();
+        sbFirst.append(ac);
+        for (int i = 0; i < (n1.length() - mid) * 2; i++) {
+            sbFirst.append('0');
+        }
+        String first = sbFirst.toString();
+
+        StringBuilder sbSecond = new StringBuilder();
+        sbSecond.append(plus(ad, bc, base));
+        for (int i = 0; i < (n1.length() - mid); i++) {
+            sbSecond.append('0');
+        }
+        String second = sbSecond.toString();
+
+        return plus(plus(first, second, base), bd, base);
     }
 
     public static String[] fillZero(String n1, String n2) {
@@ -104,7 +134,7 @@ public class App {
     }
 
     public static String mulDigit(int d1, int d2, int base) {
-        int result_NBase = (d1 * d2) / base + (d1 * d2) % base;
+        int result_NBase = (d1 * d2) / base * 10 + (d1 * d2) % base;
         return Integer.toString(result_NBase);
     }
 

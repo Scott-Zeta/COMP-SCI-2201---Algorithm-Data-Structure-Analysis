d92834f336fa3acb2f60bd1e83d1cd59e8da9839
Scott Zeta <wc070211@gmail.com>
2023-11-11 19:34:49 +1030

    convert string to number

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import sys
 
+def string_to_int(s):
+    if "0" <= s <= "9":
+        return int(s)
+    elif 'A' <= s <= 'Z':
+        return ord(s) - ord('A')
+    elif 'a' <= s <= 'z':
+        return ord(s) - ord('a') + 26
 
 def parse_input():
     if len(sys.argv) != 4:
@@ -13,7 +20,7 @@ def parse_input():
         for row in table:
             row_data = []
             for column in row:
-                row_data.append(column)
+                row_data.append(string_to_int(column))
             table_data.append(row_data)
         information.append(table_data)
     return information
@@ -26,6 +33,5 @@ def main():
     print(f"Connection: {connection}")
     print(f"Construction: {construction}")
     print(f"Destruction: {desctruction}")
-
 if __name__ == "__main__":
     main()
